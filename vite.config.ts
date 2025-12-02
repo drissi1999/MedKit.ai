9ee47af07065fa36ecce65ca3bd9721d5e9d71e2
El Houcine DRISSI <60161686+drissi1999@users.noreply.github.com>
2025-12-02 16:53:14 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/MedKit.ai/', // For GitHub Pages
-  server: {
-    port: 3001,
-    host: true
-  },
-  build: {
-    outDir: 'dist'
-  }
+  base: '/MedKit.ai/',  // EXACT repo name here
 })
